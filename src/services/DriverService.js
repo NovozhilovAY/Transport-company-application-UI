fe33acfbe7725f2ab48cd2e7f78ec19952645399
NovozhilovAY <sasha.n191@yandex.ru>
2022-03-13 12:54:58 +0300

    update DriverService

--- src/services/DriverService.js
+++ src/services/DriverService.js
@@ -2,14 +2,49 @@ import HTTP from "@/utils/HTTP";
 import {getHeaders} from "@/utils/AuthHeader";
 
 export const DriverService = {
-    getFreeDrivers
+    getFreeDrivers,
+    getAllDrivers,
+    deleteById,
+    update,
+    save
 }
 
 async function getFreeDrivers(){
-    let result = await HTTP.get("/api/drivers/free", getHeaders()).then(response => {
+    return await HTTP.get("/api/drivers/free", getHeaders()).then(response => {
         return response;
+    }).catch(error => {
+        console.log(error.response)
+    });
+}
+
+async function getAllDrivers(){
+    return await HTTP.get("/api/drivers/", getHeaders()).then(response => {
+        return response.data;
     }).catch(error=>{
         console.log(error.response)
     });
-    return result;
+}
+
+async function deleteById(id){
+    return await HTTP.delete('api/drivers/'+id,getHeaders()).then(response=>{
+        return response.status;
+    }).catch(error=>{
+        console.log(error.response);
+    });
+}
+
+async function update(driver){
+    return await HTTP.put('api/drivers', driver, getHeaders()).then(response=>{
+        return response;
+    }).catch(error=>{
+        return error.response;
+    });
+}
+
+async function save(driver){
+    return await HTTP.post('api/drivers', driver, getHeaders()).then(response=>{
+        return response;
+    }).catch(error=>{
+        return error.response;
+    });
 }
